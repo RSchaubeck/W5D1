74d290a581386affe9321b85d977ea196738fe4c
App Academy Student <email@email.com>
2020-02-17 17:05:28 -0500

    Implemented hashmap and LRU cache

--- hashmap_LRU_cache/lib/p06_lru_cache.rb
+++ hashmap_LRU_cache/lib/p06_lru_cache.rb
@@ -14,6 +14,18 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      # find node associated w/ key
+      # my_node = @map[key.hash % @map.length].find { |node| node.key == key }
+      my_node = @map[key]
+      update_node!(my_node)
+      my_node.val
+    else
+      calc!(key)
+      eject! if count > @max
+      @map[key].val
+    end
+
   end
 
   def to_s
@@ -24,12 +36,27 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+      val = @prc.call(key)
+      @store.append(key, val)
+      @map.set(key, @store.last)
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node.remove
+    tail = @store.last.next
+    old_last = @store.last
+    old_last.next = node
+    node.prev = old_last
+    node.next = tail
+    tail.prev = node
+
+    # @store.append(node.key, node.val)
   end
 
   def eject!
+    lru = @store.first
+    lru.remove
+    @map.delete(lru.key)
   end
 end
